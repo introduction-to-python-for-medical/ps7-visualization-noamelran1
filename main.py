51553437b6529bedc025ea1ddeddafb74d36fc46
noamelran1 <thenoamelran@gmail.com>
2024-12-16 09:33:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
+
+
+# Import libraries
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import fetch_openml
 
-data = fetch_openml(name='wine', version=1, as_frame=True)
+
+data = fetch_openml(name='Diabates', version=1, as_frame=True)
+
+
 print(data.DESCR)
 
 df = data.frame
+
+df.sample(5)
+
 df.describe()
+
 df.dtypes
 
+
 features = df.columns
-selected_features = [features[9], features[1], features[2], features[3], features[4]]
+selected_features = [features[0], features[1], features[2], features[3], features[7]]
+
 features = list(df.columns)
 print("Available features:", features)
-selected_features = [features[9], features[1], features[2], features[3], features[4]]
+selected_features = [features[0], features[1], features[2], features[3], features[7]]
 print("Selected features: ", selected_features)
 
 plt.hist(df[selected_features[0]], bins = 5, color = 'skyblue', edgecolor = 'black')
+
+
 fig, axs  = plt.subplots(1, len(selected_features), figsize = (20,3))
 
 for ax, f in zip(axs, selected_features):
     ax.hist(df[f], bins=5, color='skyblue', edgecolor='black')
     ax.set_xlabel(f)
 
-x = df[selected_features[3]]
-y = df[selected_features[4]]
+x = df[selected_features[4]]
+y = df[selected_features[2]]
 
 plt.scatter(x, y, alpha=0.5)
-plt.xlabel(selected_features[3])
-plt.ylabel(selected_features[4])
+plt.xlabel(selected_features[4])
+plt.ylabel(selected_features[2])
 plt.show()
+
 
 reference_feature = selected_features[4]
 y = df[reference_feature]
@@ -42,8 +57,9 @@ for ax, f in zip(axs, features):
 
 plt.show()
 
-reference_feature = selected_features[3]  # The reference feature
-comparison_feature = selected_features[4]  # A feature to compare to
+
+reference_feature = selected_features[4]  # The reference feature
+comparison_feature = selected_features[2]  # A feature to compare to
 
 # Create a scatter plot for the selected pair
 plt.figure(figsize=(8, 6))
@@ -53,6 +69,8 @@ plt.ylabel(comparison_feature)
 
 # Save the plot as an image file
 plt.savefig('correlation_plot.png')
+
+plt.show()
 
 plt.show()
 
